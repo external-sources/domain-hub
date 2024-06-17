3c2011ca44f8969b5992973d7c5565069a6f9228
Spirillen <pdns@protonmail.com>
2024-06-17 17:41:21 +0200

    Publish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, you");
+    print!("Best regards My Privacy DNS ");
+    println!("https://www.mypdns.org");
+    println!("This is a namespace reservation");
 }
